e6cf3925f457742e273c6e3d5ca41c73b0a62b12
Chris Short <devcshort@outlook.com>
2018-07-30 17:15:05 -0700

    Merge branch 'api' into dev
    Removed example files

--- server/controllers/authentication.js
+++
@@ -1,8 +0,0 @@
-// example controller file
-const AuthController = new Object();
-
-AuthController.getRoot = function(req, res, next) {
-    res.send('Hello, world!');
-}
-
-module.exports = AuthController;
