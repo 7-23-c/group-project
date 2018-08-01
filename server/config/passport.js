f510f7134760ed53dc5de27b9816eb62d603a6ef
Chris Short <devcshort@outlook.com>
2018-07-31 18:38:03 -0700

    removed some lines from passport that were specific to sessions, which we are not using in our application

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,26 +3,15 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-      
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
-});
-
 passport.use('local-registration', new LocalStrategy({
     usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
+    passwordField: 'password'
 },
-    function(req, email, password, done) {
+    function(email, password, done) {
         User.findOne({ email: email }, function (err, user) {
             if (err) { return done(err); }
             if (user) {
-                return done(null, false, req.flash('registerMessage', 'Email already taken'));
+                return done(null, false);
             }
             var newUser = new User();
             newUser.email = email;
