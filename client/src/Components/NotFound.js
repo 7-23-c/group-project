b96e01384775b89f223f1d05cd3ce3631237d264
Chris Short <devcshort@outlook.com>
2018-08-04 20:49:10 -0700

    updated 404 page not found

--- client/src/Components/NotFound.js
+++ client/src/Components/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+    function goBack() {
+        window.history.back();
+    }
     return (
-        <div>
-            <h1>Page Not Found</h1>
+        <div className="NotFound">
+            <div>
+                <h1>Whoops! Looks like you came across a dead end.</h1>
+                <h2>Go <span onClick={goBack}>Back</span> or Go <Link to="/">Home</Link></h2>
+            </div>
         </div>
     )
 }
