98cb2843e0420eda73feb272a062e66a84a2d843
Chris Short <devcshort@outlook.com>
2018-08-05 10:19:53 -0700

    Merge branch 'staging' into dev
    updated spacing

--- server/config/passport.js
+++ server/config/passport.js
@@ -10,55 +10,55 @@ passport.use('local-registration', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 },
-  function (req, email, password, done) {
-    User.findOne({ 'local.email': email }, function (err, user) {
-      if (err) {
-        return done(null, false, {
-          error: 'Something unexpected happen. Please try again.'
-        });
-      }
+    function (req, email, password, done) {
+        User.findOne({ 'local.email': email }, function (err, user) {
+            if (err) {
+                return done(null, false, {
+                    error: 'Something unexpected happen. Please try again.'
+                });
+            }
 
-      if (user) {
-        return done(null, false, {
-          error: 'That email address is already in use.'
-        });
-      }
+            if (user) {
+                return done(null, false, {
+                    error: 'That email address is already in use.'
+                });
+            }
 
-      // begin validation checks
-      var errors = [];
+            // begin validation checks
+            var errors = [];
 
-      if (!req.body.fName) {
-        errors.push('First name must not be left blank.');
-      }
+            if (!req.body.fName) {
+                errors.push('First name must not be left blank.');
+            }
 
-      if (!req.body.lName) {
-        errors.push('Last name must not be left blank.');
-      }
+            if (!req.body.lName) {
+                errors.push('Last name must not be left blank.');
+            }
 
-      if (!req.body.username) {
-        errors.push('Username must not be left blank.');
-      }
+            if (!req.body.username) {
+                errors.push('Username must not be left blank.');
+            }
 
-      if (errors.length > 0) {
-        return done(null, false, { errors: errors });
-      }
+            if (errors.length > 0) {
+                return done(null, false, { errors: errors });
+            }
       
-      var newUser = new User();
+            var newUser = new User();
 
-      newUser.local.email = email;
-      newUser.local.password = newUser.generateHash(password);
-      newUser.name.first = req.body.fName;
-      newUser.name.last = req.body.lName;
-      newUser.username = req.body.username;
+            newUser.local.email = email;
+            newUser.local.password = newUser.generateHash(password);
+            newUser.name.first = req.body.fName;
+            newUser.name.last = req.body.lName;
+            newUser.username = req.body.username;
 
-      newUser.save(function (err) {
-        if (err) throw err;
-        return done(null, newUser, {
-          message: 'User created successfully!'
+            newUser.save(function (err) {
+                if (err) throw err;
+                return done(null, newUser, {
+                    message: 'User created successfully!'
+                });
+            });
         });
-      });
-    });
-  }
+    }
 ));
 
 passport.use('generate-token', new LocalStrategy({
