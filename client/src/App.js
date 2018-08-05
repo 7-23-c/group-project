5a85e6faa4fd1debe00e854426278005fec79125
Chris Short <devcshort@outlook.com>
2018-08-05 10:30:53 -0700

    updated spacing on front end

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/register" component={Registration} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/logout" component={Logout} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    );
-  }
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/register" component={Registration} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/logout" component={Logout} />
+                <Route path="*" component={NotFound} />
+            </Switch>
+        );
+    }
 }
 
 export default App;
