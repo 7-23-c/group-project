ee71ef4e14afc84a43d5f2de79d1add11627da6e
Bladimir Hernandez <hernandezblad95@gmail.com>
2018-08-07 18:30:48 -0500

    Updated the Friends UI component
    this component lists the users' Friends and Pending-Friends. So far, this component reads in fixed arrays. Note: this component still needs to read the data from the database.

--- client/src/Components/friends.js
+++ client/src/Components/friends.js
@@ -13,34 +13,40 @@ import React, { Component } from "react";
 import Axios from 'axios';
 import './Friends.css';
 
-class Friends extends Component {
+//friends list:
+const friends = ["Friend #1", "Friend #2", "Friend #3", "Friend #4"];
+const listFriends = friends.map(friend => (<li>{friend}</li>));
+const FriendsList = props => (
+    <li> {props.friends} </li>
+);
 
-    // const friends = ["friend #1", "friend #2", "friend #3"];
-    // const pendingFriends = ["Pending #1", "Pending #2", "Pending #3"]
+//Pending Friends list:
+const pendingFriends = ["Pending #1", "Pending #2", "Pending #3","Pending #4"];
+const listPendingFriends = pendingFriends.map(pendingFriend => (<li>{pendingFriend}</li>));
+const PendingFriendsList = props => (
+    <li> {props.pendingFriends}</li>
+);
+
+class Friends extends Component {
 
     render() {
         return (
             <div className="Friends">
-                <div className="Friends-only">
-                Friends: 
-                    <div>
-                        {/* TODO: lists the items that are in the 'friends' array */}
-
-                        friend #1,
-                        friend #2,
-                        friend #3
+                <div className="Friends-only-Header">
+                    Friends
+                    <div className="Friends-only-List">
+                        <FriendsList friends={listFriends} />
                     </div>
                 </div>
-                <div className="Pending-only">
-                    Pending Friends:
-                    <div>
-                        Pending Friend #1,
-                        Pending Friend #2,
-                        Pending Friend #3
+                <div className="Pending-Friends">
+                    <div className="Pending-only-Header">
+                        Pending Friends
+                        <div className="Pending-only-List">
+                            <PendingFriendsList pendingFriends={listPendingFriends} />
+                        </div>
                     </div>
                 </div>
             </div>
-
         );
     }
 }
