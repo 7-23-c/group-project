96f9e479fd115e01c655e0da0b04bb8fce0a3178
Bladimir Hernandez <hernandezblad95@gmail.com>
2018-08-07 10:52:20 -0500

    layout
    basic layout of the friends component. This component has two sections. A section of friends and a section of pending friends.

--- client/src/Components/friends.js
+++ client/src/Components/friends.js
@@ -4,3 +4,44 @@
 
 // this component will manage the list: update, delete, ... 
 
+//Stuff to import:
+//  UI stuff such as a list format
+//  friends from the user
+//  pending friends from the user
+
+import React, { Component } from "react";
+import Axios from 'axios';
+import './Friends.css';
+
+class Friends extends Component {
+
+    // const friends = ["friend #1", "friend #2", "friend #3"];
+    // const pendingFriends = ["Pending #1", "Pending #2", "Pending #3"]
+
+    render() {
+        return (
+            <div className="Friends">
+                <div className="Friends-only">
+                Friends: 
+                    <div>
+                        {/* TODO: lists the items that are in the 'friends' array */}
+
+                        friend #1,
+                        friend #2,
+                        friend #3
+                    </div>
+                </div>
+                <div className="Pending-only">
+                    Pending Friends:
+                    <div>
+                        Pending Friend #1,
+                        Pending Friend #2,
+                        Pending Friend #3
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+export default Friends;
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Friends from './Components/Friends';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-document.getElementById('root'));
+ReactDOM.render(<Friends />, document.getElementById('root'));
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>,
+// document.getElementById('root'));
 registerServiceWorker();
