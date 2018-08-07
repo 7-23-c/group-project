0504efd39f64b0c65e6965a00ca154e394eef6fc
Bladimir Hernandez <hernandezblad95@gmail.com>
2018-08-06 23:46:09 -0500

    TODO
    Made a list of the fucntions the component is going to perform.

---
+++ client/src/Components/friends.js
@@ -0,0 +1,6 @@
+// Stuff to do:
+// this component will show a list of "friends" the user is with.
+// this component will show a list of "pending friends" of the userInfo
+
+// this component will manage the list: update, delete, ... 
+
