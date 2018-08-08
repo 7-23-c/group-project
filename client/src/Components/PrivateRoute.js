364ba553597f165ccfe57b6854cacb465d4c3be2
Chris Short <devcshort@outlook.com>
2018-08-08 15:53:21 -0700

    fixed a bug with the private route component

--- client/src/Components/PrivateRoute.js
+++ client/src/Components/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 const PrivateRoute = (props) => {
     if (props.isLoggedIn) {
         return (
-            <Route exact path={this.props.path} component={this.props.component} />
+            <Route exact path={props.path} component={props.component} />
         )
     } else {
         return (
