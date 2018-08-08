c5614bf7186117c8517b1c04626d29e250a653be
Chris Short <devcshort@outlook.com>
2018-08-07 18:41:28 -0700

    updated beacon routes and controllers

--- server/controllers/beacons.js
+++ server/controllers/beacons.js
@@ -1,57 +1,40 @@
 const BeaconController = new Object();
 const Beacon = require('../models/beacon');
 const jwtSecret = process.env.JWT_SECRET || process.env.JWT_SECRET_DEV;
-// add in all controller methods
 
 BeaconController.createNewBeacon = function(req, res, next) {
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Field Cannot Be Empty"
-        });
-    }
-    const beacon = new Beacon({
+    var beacon = new Beacon({
         title: req.body.title || "Untitled Beacon", 
-        location:{
-            longitude: req.body.latitude,
-            latitude: req.body.longitude
+        location: {
+            longitude: req.body.longitude,
+            latitude: req.body.latitude
         } 
     });
     
     beacon.save()
     .then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while saving the Beacon."
+        res.json({ success: 'Beacon created successfully!' });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: 'An error occurred while saving the Beacon.'
         });
     });   
 }
 
-BeaconController.findAllBeacon = (req, res, next) => {
-
+BeaconController.findAllBeacons = (req, res, next) => {
     var token = req.header.authorization.split(' ')[1];
+
     jwt.verify(token, jwtSecret, function(err, decoded){
-
-        
-    Beacon.find({created_by: decoded.id})
-    .then(beacons => {
-        res.json({beacons: beacons});
-    }).catch(err => {
-        res.status(500).json({
-            message: err.message || "Some error occurred while retrieving beacons."
+        Beacon.find({created_by: decoded.id})
+        .then(beacons => {
+            res.json({ beacons: beacons });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "An error occurred while retrieving beacons."
+            });
         });
-    
-    });
-
-
-
-    /*Beacon.find()
-    .then(beacons => {
-        res.send(beacons);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving beacons."
-        });*/
     });
 };
 
@@ -60,70 +43,65 @@ BeaconController.findOneBeacon = (req, res, next) => {
     .then(beacon => {
         if(!beacon) {
             return res.status(404).json({
-                message: "Beacon not found with id " + req.params.id
+                error: "Beacon not found."
             });            
         }
-        res.json(beacon);
-    }).catch(err => {
+        res.json({ beacon: beacon });
+    })
+    .catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).json({
-                message: "Beacon not found with id " + req.params.id
+                error: "Beacon not found."
             });                
         }
         return res.status(500).json({
-            message: "Error retrieving Beacon with id " + req.params.id
+            error: "Error retrieving Beacon."
         });
     });
 };
 
 BeaconController.updateBeacon = (req, res, next) => {
-    // Validate Request
-    if(!req.body.content) {
-        return res.status(400).json({
-            message: "This cannot be empty"
-        });
-    }
-
     // Find beacon and update it with the request body
     Beacon.findByIdAndUpdate(req.params.id, {
         title: req.body.title || "Untitled Beacon",
-        location:{
-            longitude: req.body.latitude,
-            latitude: req.body.longitude
+        location: {
+            longitude: req.body.longitude,
+            latitude: req.body.latitude
         }
     }, {new: true})
     .then(beacon => {
         if(!beacon) {
             return res.status(404).json({
-                message: "Note not found with id " + req.params.id
+                error: "Beacon not found. "
             });
         }
-        res.json(beacon);
-    }).catch(err => {
+        res.json({ beacon: beacon });
+    })
+    .catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).json({
-                message: "Note not found with id " + req.params.id
+                error: "Beacon not found."
             });                
         }
         return res.status(500).json({
-            message: "Error updating note with id " + req.params.id
+            error: "Error updating beacon."
         });
     });
 };
 
 BeaconController.deleteBeacon = function(req, res, next) {
-        Beacon.findById(req.params.id,function(err, beacon) {
+    Beacon.findById(req.params.id, function(err, beacon) {
+        if (err) {
+            return res.json({ error: 'Something unexpected happened.' });
+        }
+
+        beacon.remove(function(err) {
             if (err) {
                 return res.json({ error: 'Something unexpected happened.' });
             }
-
-            beacon.remove(function(err) {
-                if (err) {
-                    return res.json({ error: 'Something unexpected happened.' });
-                }
-                return res.json({ success: 'Beacon deleted successfully!' });
-            });
+            return res.json({ success: 'Beacon deleted successfully!' });
         });
-    }
+    });
+}
 
 module.exports = BeaconController;
--- server/routes/beacons.js
+++ server/routes/beacons.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const BeaconController = require('../controllers/beacons');
 const Authorize = require('../helpers/authorize');
 
-router.post('/beacons/:id', Authorize, (req, res, next) => BeaconController.createNewBeacon(req, res, next));
+router.post('/beacons/:id', Authorize, (req, res, next) =>
+BeaconController.createNewBeacon(req, res, next));
 
-router.get('/beacons', beacon.findAll,(req, res, next) => BeaconController.findAllBeacon(req, res, next));
+router.get('/beacons', (req, res, next) =>
+BeaconController.findAllBeacons(req, res, next));
 
-router.get('/beacons/:id', beacon.findOne,(req, res, next) => BeaconController.findOneBeacon(req, res, next));
+router.get('/beacons/:id', (req, res, next) =>
+BeaconController.findOneBeacon(req, res, next));
 
-router.put('/beacons/:id', beacon.update,(req, res, next) => BeaconController.updateBeacon(req, res, next) );
+router.put('/beacons/:id', (req, res, next) =>
+BeaconController.updateBeacon(req, res, next) );
 
 module.exports = router;
