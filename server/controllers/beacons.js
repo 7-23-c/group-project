86c05adab82ec9b75600119617279452590b041b
Unknown <adeshinayemi@gmail.com>
2018-08-07 21:26:03 -0400

    Added beacon controllers and routes

--- server/controllers/beacons.js
+++ server/controllers/beacons.js
@@ -1,6 +1,6 @@
 const BeaconController = new Object();
 const Beacon = require('../models/beacon');
-
+const jwtSecret = process.env.JWT_SECRET || process.env.JWT_SECRET_DEV;
 // add in all controller methods
 
 BeaconController.createNewBeacon = function(req, res, next) {
@@ -24,9 +24,92 @@ BeaconController.createNewBeacon = function(req, res, next) {
         res.status(500).send({
             message: err.message || "Some error occurred while saving the Beacon."
         });
-    });
-    
+    });   
 }
+
+BeaconController.findAllBeacon = (req, res, next) => {
+
+    var token = req.header.authorization.split(' ')[1];
+    jwt.verify(token, jwtSecret, function(err, decoded){
+
+        
+    Beacon.find({created_by: decoded.id})
+    .then(beacons => {
+        res.json({beacons: beacons});
+    }).catch(err => {
+        res.status(500).json({
+            message: err.message || "Some error occurred while retrieving beacons."
+        });
+    
+    });
+
+
+
+    /*Beacon.find()
+    .then(beacons => {
+        res.send(beacons);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving beacons."
+        });*/
+    });
+};
+
+BeaconController.findOneBeacon = (req, res, next) => {
+    Beacon.findById(req.params.id)
+    .then(beacon => {
+        if(!beacon) {
+            return res.status(404).json({
+                message: "Beacon not found with id " + req.params.id
+            });            
+        }
+        res.json(beacon);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({
+                message: "Beacon not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).json({
+            message: "Error retrieving Beacon with id " + req.params.id
+        });
+    });
+};
+
+BeaconController.updateBeacon = (req, res, next) => {
+    // Validate Request
+    if(!req.body.content) {
+        return res.status(400).json({
+            message: "This cannot be empty"
+        });
+    }
+
+    // Find beacon and update it with the request body
+    Beacon.findByIdAndUpdate(req.params.id, {
+        title: req.body.title || "Untitled Beacon",
+        location:{
+            longitude: req.body.latitude,
+            latitude: req.body.longitude
+        }
+    }, {new: true})
+    .then(beacon => {
+        if(!beacon) {
+            return res.status(404).json({
+                message: "Note not found with id " + req.params.id
+            });
+        }
+        res.json(beacon);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({
+                message: "Note not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).json({
+            message: "Error updating note with id " + req.params.id
+        });
+    });
+};
 
 BeaconController.deleteBeacon = function(req, res, next) {
         Beacon.findById(req.params.id,function(err, beacon) {
--- server/routes/beacons.js
+++ server/routes/beacons.js
@@ -5,4 +5,10 @@ const Authorize = require('../helpers/authorize');
 
 router.post('/beacons/:id', Authorize, (req, res, next) => BeaconController.createNewBeacon(req, res, next));
 
+router.get('/beacons', beacon.findAll,(req, res, next) => BeaconController.findAllBeacon(req, res, next));
+
+router.get('/beacons/:id', beacon.findOne,(req, res, next) => BeaconController.findOneBeacon(req, res, next));
+
+router.put('/beacons/:id', beacon.update,(req, res, next) => BeaconController.updateBeacon(req, res, next) );
+
 module.exports = router;
