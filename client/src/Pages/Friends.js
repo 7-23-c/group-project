112ca1c6f55a1c07edf8f1b51c0efac3fdaf4c15
Bladimir Hernandez <hernandezblad95@gmail.com>
2018-08-08 22:46:22 -0500

    updated the list UI format
    changed the format of the list using material-ui

--- client/src/Pages/Friends.js
+++ client/src/Pages/Friends.js
@@ -5,44 +5,70 @@
 // this component will manage the list: update, delete, ... 
 
 //Stuff to import:
-//  UI stuff such as a list format
 //  friends from the user
 //  pending friends from the user
 
 import React, { Component } from "react";
 import Axios from 'axios';
 import './Friends.css';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 //friends list:
+
 const friends = ["Friend #1", "Friend #2", "Friend #3", "Friend #4"];
-const listFriends = friends.map(friend => (<li>{friend}</li>));
+const listFriends = friends.map(friend => (
+    <List >
+        <ListItem button >
+            <ListItemText primary={friend} />
+        </ListItem>
+    </List>)
+    )
+;
 const FriendsList = props => (
-    <li> {props.friends} </li>
+    <div className="Friends-only-List"> 
+        {props.friends} 
+    </div>
 );
+// ***************************************************
 
 //Pending Friends list:
 const pendingFriends = ["Pending #1", "Pending #2", "Pending #3","Pending #4"];
-const listPendingFriends = pendingFriends.map(pendingFriend => (<li>{pendingFriend}</li>));
-const PendingFriendsList = props => (
-    <li> {props.pendingFriends}</li>
+const listPendingFriends = pendingFriends.map(pendingFriend => (
+    <List>
+        <ListItem button component={pendingFriend.id} href="">
+            <ListItemText primary={pendingFriend}  />
+        </ListItem>
+    </List>
+    )
 );
+const PendingFriendsList = props => (
+    <div className="Pending-only-List" key={props.pendingFriends.id} >
+        {props.pendingFriends}
+    </div>
+
+);
+// ***************************************************
+
 
 class Friends extends Component {
 
     render() {
+
         return (
             <div className="Friends">
                 <div className="Friends-only-Header">
                     Friends
                     <div className="Friends-only-List">
-                        <FriendsList friends={listFriends} />
+                        <FriendsList  friends={listFriends} />
                     </div>
                 </div>
                 <div className="Pending-Friends">
                     <div className="Pending-only-Header">
                         Pending Friends
                         <div className="Pending-only-List">
-                            <PendingFriendsList pendingFriends={listPendingFriends} />
+                            <PendingFriendsList pendingFriends={listPendingFriends}  />
                         </div>
                     </div>
                 </div>
