c7b4b2e89b5aa88001b84f06946838531fc7a70e
Chris Short <devcshort@outlook.com>
2018-08-09 20:10:32 -0700

    updated error handling

--- server/controllers/tokens.js
+++ server/controllers/tokens.js
@@ -6,12 +6,12 @@ require('../config/passport');
 TokenController.getToken = function(req, res, next) {
     passport.authenticate('generate-token', function(err, user, info) {
         if (err) {
-            return res.json(info);
+            return res.status(500).json(info);
         }
         if (!user) {
-            return res.json(info)
+            return res.status(400).json(info)
         }
-        return res.json(info);
+        return res.status(200).json(info);
     })(req, res, next);
 }
 
