a7d51fcb34b0ebae19a74046cb0a6edd8062e4e4
auswoodall <austin.woodall72@gmail.com>
2018-08-15 18:22:04 -0500

    initial commit

--- client/src/Pages/Map.js
+++ client/src/Pages/Map.js
@@ -1,13 +1,23 @@
 // import react components
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
 // import css
 import './Map.css';
 
 class Map extends Component {
     render() {
+        const googleMap = withGoogleMap(props => (
+            <GoogleMap defaultCenter = { { lat: 40.756795, ln: -73.954298 } } defaultZoom = { 13 }>
+            </GoogleMap>
+        ));
+
         return (
-            <h3>Map</h3>
+            <div>
+                <googleMap containerElement={ <div style={ { height: '500px', width:'500px' } } /> }
+                    mapElement={ <div style={{ height: '100%' }}/>}
+                />
+            </div>
         )
     }
 }
