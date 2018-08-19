6acf8b672c66c88012ae84f2e4b03f76e0a9c0ba
Chris Short <devcshort@outlook.com>
2018-08-18 21:44:04 -0700

    Changed home cpmponent to hold our landing page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
-    if (localStorage.getItem('token') !== null) {
-        return <Redirect to='/dashboard' />
-    } else {
-        return <Redirect to='/login' />
+import AppStoreBadge from '../../Images/app-store-badge.svg';
+import PlayStoreBadge from '../../Images/play-store-badge.svg';
+
+import './Home.css';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <nav className="main-navigation">
+                    <div className="brand">
+                        <Link to="/">Beacons</Link>
+                    </div>
+                    <ul>
+                        <li><Link to="/login">Login</Link></li>
+                        <li>Or</li>
+                        <li><Link to="/register">Register</Link></li>
+                    </ul>
+                </nav>
+
+                <section className="main">
+                    <div className="overlay">
+                        <div>
+                            <h1>Experience a New Way to Share Memories</h1>
+                            <Link to="/"><img className="badge one" src={AppStoreBadge} alt="Get the app on the app store." /></Link>
+                            <Link to="/"><img className="badge" src={PlayStoreBadge} alt="Get the app on the play store." /></Link>
+                        </div>
+                    </div>
+                </section>
+            </React.Fragment>
+        )
     }
 }
 
