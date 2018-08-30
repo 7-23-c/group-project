9b26da00dd8ac0a71d0d5aaee1ee0beaf29cf1ea
Chris Short <devcshort@outlook.com>
2018-08-30 15:19:31 -0700

    bugfix

--- server/config/passport.js
+++ server/config/passport.js
@@ -12,61 +12,61 @@ passport.use('local-registration', new LocalStrategy({
 },
     function (req, email, password, done) {
         User.findOne({ 'local.email': email })
-            .then(user => {
-                if (user) {
-                    return done(null, false, {
-                        error: 'That email address is already in use.'
-                    });
-                }
-                
-                return User.findOne({ username: req.body.username });
-            })
-            .then(user => {
-                if (user) {
-                    return done(null, false, {
-                        error: 'That username is already in use.'
-                    });
-                }
-
-                // begin validation checks
-                var errors = [];
-
-                if (!req.body.fName) {
-                    errors.push('First name must not be left blank.');
-                }
-
-                if (!req.body.lName) {
-                    errors.push('Last name must not be left blank.');
-                }
-
-                if (!req.body.username) {
-                    errors.push('Username must not be left blank.');
-                }
-
-                if (errors.length > 0) {
-                    return done(null, false, { errors: errors });
-                }
-        
-                var newUser = new User();
-
-                newUser.local.email = email;
-                newUser.local.password = newUser.generateHash(password);
-                newUser.name.first = req.body.fName;
-                newUser.name.last = req.body.lName;
-                newUser.username = req.body.username;
-
-                return newUser.save();
-            })
-            .then(() => {
-                return done(null, newUser, {
-                    message: 'User created successfully!'
-                });
-            })
-            .catch(() => {
+        .then(user => {
+            if (user) {
                 return done(null, false, {
-                    error: 'Something unexpected happen. Please try again.'
+                    error: 'That email address is already in use.'
                 });
+            }
+
+            return User.findOne({ username: req.body.username });
+        })
+        .then(user => {
+            if (user) {
+                return done(null, false, {
+                    error: 'That username is already in use.'
+                });
+            }
+
+            // begin validation checks
+            var errors = [];
+
+            if (!req.body.fName) {
+                errors.push('First name must not be left blank.');
+            }
+
+            if (!req.body.lName) {
+                errors.push('Last name must not be left blank.');
+            }
+
+            if (!req.body.username) {
+                errors.push('Username must not be left blank.');
+            }
+
+            if (errors.length > 0) {
+                return done(null, false, { errors: errors });
+            }
+        
+            var newUser = new User();
+
+            newUser.local.email = email;
+            newUser.local.password = newUser.generateHash(password);
+            newUser.name.first = req.body.fName;
+            newUser.name.last = req.body.lName;
+            newUser.username = req.body.username;
+
+            return newUser.save();
+        })
+        .then(() => {
+            return done(null, true, {
+                message: 'User created successfully!'
             });
+        })
+        .catch(() => {
+            return done(null, false, {
+                error: 'Something unexpected happened. Please try again.'
+            });
+        });
     }
 ));
 
